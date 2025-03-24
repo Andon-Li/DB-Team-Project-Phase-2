87b877c27e165f15a7112163e17a17548aa7e333
Michael Procyk <mprocyk022304@gmail.com>
2025-03-24 16:01:59 -0400

    page switching & start login auth

--- main.py
+++ main.py
@@ -12,3 +12,25 @@ if __name__ == '__main__':
 def index():
     return render_template('login.html')
 
+@app.route("/signuppage")
+def signuppage():
+    return render_template('signup.html')
+
+@app.route("/loginpage")
+def loginpage():
+    return render_template('login.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        if not email or not password:
+            errorMessage = "Ensure email and password fields are filled"
+            return render_template('login.html', errorMessage=errorMessage)
+        else:
+            #database auth
+            return
+
+    return render_template('login.html')
