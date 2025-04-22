d73963f8c0a0ead7f2eac548ae3b76243d9dceb2
Andon Li <andonli281@gmail.com>
2025-04-21 21:48:30 -0400

    started importing for Client table from data

--- import.py
+++ import.py
@@ -6,6 +6,7 @@ from hashlib import sha256
 connection = sqlite3.connect("nittanybusiness.db")
 cursor = connection.cursor()
 
+
 print("Importing Users.csv data the users table of nittanybusiness.db")
 
 cursor.execute("""
@@ -29,3 +30,28 @@ with open("./data/Users.csv", newline='') as f:
 
     connection.commit()
 
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS clients (
+        userID TEXT PRIMARY KEY,
+        cardNumber INTEGER,
+        street TEXT,
+        zipCode INTEGER,
+        businessName TEXT,
+        activeStatus INTEGER
+    );""")
+# activeStatus: 1 is active, 0 is inactive
+
+with open("./data/.csv", newline='') as f:
+    reader = csv.reader(f)
+    
+    reader.__next__()  # skip first line containing column titles
+    for row in reader:
+        userID = row[0]
+        passwordHash = sha256(bytes(row[1], "utf-8")).hexdigest()
+            
+        cursor.execute(f"""
+            INSERT INTO users VALUES ('{userID}', '{passwordHash}');
+        """)
+
+    connection.commit()
